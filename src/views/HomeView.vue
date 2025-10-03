35681aec33ff1321e6c4152f8494fee28de98d30
Rage179 <maxime.meteye@gmail.com>
2025-10-03 20:44:51 +0200

    feat: integrate SASS for styling and update grid system
    - Added SASS as a dependency and changed main stylesheet from CSS to SCSS.
    - Updated grid layout styles to improve responsiveness and structure.
    - Removed deprecated gridcopy.css file.
    - Enhanced HomeView.vue to utilize new grid classes for better layout management.
    - Added media queries for responsive design in grid.css.
    - Created a TODO for integrating the SASS stylesheet.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,10 +6,10 @@
         <div class="--container">
 			<div class="--row-1 --row-justify-left">
 				<div class="--col">Test</div>
-				<section class="--col --offset-1">contains the meat of the page</section>
+				<section class="--col --col-offset-1 --col-s-offset-0 --col-s-12">contains the meat of the page</section>
 			</div>
-			<section class="--row-1 --offset-1">
-				<div class=" --col-1 --offset-1">3</div>
+			<section class="--row-1">
+				<div class=" --col-1 --col-m-6 ">3</div>
 			</section>
         </div>
     </div>
