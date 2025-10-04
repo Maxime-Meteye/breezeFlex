80df6abcdfbd0e7bfa84b8df2f787bf4c7fc2846
Rage179 <maxime.meteye@gmail.com>
2025-10-04 18:39:31 +0200

    Grid working with sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
         </h1>
         <div class="--container">
 			<div class="--row-1 --row-justify-left">
-				<div class="--col">Test</div>
+				<div class="--col debug">Test</div>
 				<section class="--col --col-offset-1 --col-s-offset-0 --col-s-12">contains the meat of the page</section>
 			</div>
 			<section class="--row-1">
