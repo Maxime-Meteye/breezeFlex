717667068d650542c153545be10cf00319f7a4ee
Rage179 <maxime.meteye@gmail.com>
2025-10-04 20:02:02 +0200

    removed class prefix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-<nav class="container">
-  <button popovertarget="navmenu" class="hamburger row">=</button>
-  <nav id='navmenu' popover class="sidenav lpad-xs row" :class="{ 'sidenav-open': is_menu_open}">
-    <RouterLink to="/" class="col-1">Go to Home</RouterLink>
-    <RouterLink to="/ourlocation"class="col-1">Find us</RouterLink>
-    <RouterLink to="/menu"class="col-2">Our Menu</RouterLink>
-    <RouterLink to="/contact" class="col-1">Contact us</RouterLink>
+<nav >
+  <button popovertarget="navmenu" class="hamburger">=</button>
+  <nav id='navmenu' popover :class="{ 'sidenav-open': is_menu_open}">
+    <RouterLink to="/" >Go to Home</RouterLink>
+    <RouterLink to="/ourlocation">Find us</RouterLink>
+    <RouterLink to="/menu">Our Menu</RouterLink>
+    <RouterLink to="/contact">Contact us</RouterLink>
   </nav>
 </nav>
 <main>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,21 @@
         <h1>
             home
         </h1>
-        <div class="--container">
-			<div class="--row-1 --row-justify-left">
-				<div class="--col debug">Test</div>
-				<section class="--col --col-offset-1 --col-s-offset-0 --col-s-12">contains the meat of the page</section>
+        <div class="container">
+			<div class="row-1 row-justify-left">
+				<div class="col">Test</div>
+				<section class="col col-offset-1 col-s-offset-0 col-s-12">contains the meat of the page</section>
 			</div>
-			<section class="--row-1">
-				<div class=" --col-1 --col-m-6 ">3</div>
+			<section class="row-1">
+				<div class=" col-1 col-m-6 ">3</div>
 			</section>
+			<div class="row-1 row-justify-left">
+				<div class="col-6">lorqsfsqfqsqsf</div>
+				<div class="col-6">lorqsfsq22222fqsqsf</div>
+			</div>
+			<div class="row-1 row-justify-left">
+				<div class="col-12">lorqsfsqfqsqsf</div>
+			</div>
         </div>
     </div>
 </template>
